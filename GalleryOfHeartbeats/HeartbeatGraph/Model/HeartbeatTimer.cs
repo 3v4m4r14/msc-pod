16d62d7c6f3b3e221847d626ed7dd94d513c96ac
Eva Maria Veitmaa <e.m.veitmaa@gmail.com>
2020-05-21 23:49:13 -0700

    Separated timer for triggering sensory playback events
    (Dynamics of heartbeat tempo change do not work properly yet.)

---
+++ GalleryOfHeartbeats/HeartbeatGraph/Model/HeartbeatTimer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalleryOfHeartbeats.Model
+{
+    class HeartbeatTimer
+    {
+        private const int ONE_MINUTE = 60000;
+        private const int SLOW_HEARTBEAT_INTERVAL = 900;
+        private const int FAST_HEARTBEAT_INTERVAL = 700;
+        private int heartbeatInterval = 0;
+
+        private DateTime prevHeartbeatTime = DateTime.Now;
+        private DateTime curTime;
+
+        public HeartbeatTimer() { }
+
+        private bool IntervalHasPassed()
+        {
+            return (curTime - prevHeartbeatTime).TotalMilliseconds >= heartbeatInterval;
+        }
+
+        public bool TimeForHeartbeat(int heartrate)
+        {
+            curTime = DateTime.Now;
+
+            heartbeatInterval = (int)Math.Ceiling((double) ONE_MINUTE / heartrate);
+
+
+            if (IntervalHasPassed())
+            {
+                Console.WriteLine("Interval has passed: " + heartbeatInterval + " " + (curTime - prevHeartbeatTime).TotalMilliseconds);
+                prevHeartbeatTime = DateTime.Now;
+                return true;
+            }
+            return false;
+        }
+    }
+}

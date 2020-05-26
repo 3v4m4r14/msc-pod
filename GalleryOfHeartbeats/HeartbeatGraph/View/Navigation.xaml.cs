84f96d1c15cd0fc7df23eb33aacd53aa242a5f38
Eva Maria Veitmaa <e.m.veitmaa@gmail.com>
2020-05-26 12:59:23 -0700

    Navigation UI incl a navigation drawer

---
+++ GalleryOfHeartbeats/HeartbeatGraph/View/Navigation.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GalleryOfHeartbeats.View
+{
+    /// <summary>
+    /// Interaction logic for Navigation.xaml
+    /// </summary>
+    public partial class Navigation : Window
+    {
+        public Navigation()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
